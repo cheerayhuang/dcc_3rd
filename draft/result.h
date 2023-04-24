2223e1e2eca281a0c8ccee0441a1e34c0faac42b
cheeray.huang <cheeray.huang@gmail.com>
2023-04-24 15:54:03 +0800

    add: some class to store and output results.

---
+++ draft/result.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <algorithm>
+#include <list>
+#include <iostream>
+#include <mutex>
+#include <vector>
+
+template <typename T>
+struct ResultData {
+
+    T data; 
+    unsigned int index;
+
+    using DataType = T;
+};
+
+template <typename T>
+bool operator ==(const ResultData<T> &op1, const ResultData<T> &op2) {
+    return op1.data == op2.data;
+}
+
+template <typename T>
+bool operator <(const ResultData<T> &op1, const ResultData<T> &op2) {
+    return  op1.data < op2.data;
+}
+
+using namespace std::rel_ops;
+
+template<typename T>
+class Result {
+
+    using ResList = std::list<T>;
+    enum {
+        kMaxNumOfResultData = 10
+    };
+
+private:
+    size_t len_ = 0;
+    ResList res_list_;
+    typename Result<T>::ResList::iterator min_iter_ = res_list_.begin();
+    
+    std::mutex insert_data_mutex_;
+
+public:
+
+    Result() = default;
+
+    const ResList& GetResult() const {
+        return res_list_;
+    }
+
+    size_t Len() {
+        return len_;
+    }
+
+    void InsertResData(T&& data) {
+        if (len_ == kMaxNumOfResultData && data < *min_iter_) {
+            return ;
+        }
+
+
+        res_list_.push_front(std::move(data));
+
+        std::scoped_lock lk{insert_data_mutex_};
+        if (len_ == kMaxNumOfResultData) {
+            res_list_.erase(min_iter_); 
+        } else {
+            len_++;
+        }
+        
+        if (len_ > 1) {
+            min_iter_ = std::min_element(res_list_.begin(), res_list_.end());
+        }
+    }
+
+    using MetaDataType = typename T::DataType;
+};
+
+template <typename T>
+using AllResults = std::vector<Result<T>*>;
+
---
+++ draft/result_writer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <string>
+
+#include "result.h"
+
+
+class ResultWriter {
+
+
+private:
+    const std::string kResultFilePath{"result.csv"};
+    const std::string kHeaderRow{"seed_index,dict_index,rank"};
+
+    std::ofstream fout_;
+
+
+public:
+    
+    ResultWriter() = default;
+    ~ResultWriter() {
+        fout_.close();
+    }
+
+    void WriterHeader() {
+        fout_ = std::ofstream(kResultFilePath);
+        fout_ << kHeaderRow << std::endl;
+    }
+
+    template<typename T>
+    void Write(const AllResults<T>& all_res) {
+        WriterHeader();
+
+        for(auto i = 0; i < all_res.size(); ++i) {
+            if (all_res[i] == nullptr) continue;
+            auto rank = 0;
+            const auto& res_list = all_res[i]->GetResult();
+
+            std::for_each(res_list.begin(), res_list.end(), 
+                    [i, this, &rank](auto&& data) {
+                        fout_ << i << "," << data.index << "," << data.data << "," << rank << "\n";
+                        rank++;
+                    } 
+            );
+        }
+
+    }
+};
