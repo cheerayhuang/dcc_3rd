41e304314bef77f3c68ef30453254cc30deed991
cheeray.huang <cheeray.huang@gmail.com>
2023-04-25 22:21:05 +0800

    upt: build.sh

--- draft/build.sh
+++ draft/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-g++ main.cpp -std=c++17 -I/home/chee/3rd_libs/include -mfma -O3 -Ofast -ffast-math -fopenmp -L/home/chee/3rd_libs/lib #-static -lopenblas
+g++ main.cpp -std=c++17 -I/home/chee/3rd_libs/include -mfma -O3 -Ofast -ffast-math -fopenmp -L/home/chee/3rd_libs/lib #-ljemalloc #-static -lopenblas
+#g++ main.cpp -std=c++17 -mfma -O3 -Ofast -ffast-math -fopenmp #-static -lopenblas
