7aec796436188012bd85f38cec2f444da25dc52b
cheeray.huang <cheeray.huang@gmail.com>
2023-05-05 18:38:06 +0800

    fix: the test for Class Timer

--- src/include/timer_test.cpp
+++ src/include/timer_test.cpp
@@ -1,5 +1,6 @@
 #include "timer.h"
 #include <forward_list>
+#include <thread>
 
 auto func() {
     using secs_per_hour = std::ratio_multiply<std::ratio<1>, std::chrono::hours::period>;
@@ -8,17 +9,17 @@ auto func() {
 }
 
 int main() {
-    Timer t1; 
-    t1.start();
+    Timer t1;
+    t1.Start();
     std::cout << func() << std::endl;
-    std::cout << t1.stop().AsNanoseconds() << std::endl;
+    std::cout << t1.Stop().AsNanoseconds() << std::endl;
     auto flags = std::cout.flags();
     std::cout << std::fixed << std::setprecision(9) << t1.AsSeconds() << std::endl;
     std::cout << t1.AsMiliseconds() << std::endl;
     std::cout << t1.AsMicroseconds() << std::endl;
     std::cout << t1 << std::endl;
     //std::cout.flags(flags);
-    
+
     std::cout << t1.NowAsNanoseconds() << std::endl;
 
     return 0;
