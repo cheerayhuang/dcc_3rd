045719d4cd37c770ba5638f68ea7887629fcbbf6
cheeray.huang <cheeray.huang@gmail.com>
2023-05-11 15:18:58 +0800

    upt: build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #g++ main.cpp  -std=c++17 -I/home/chee/3rd_libs/include -mfma -O3 -Ofast -ffast-math   -fopenmp -Wl,-Bstatic -L/home/chee/3rd_libs/lib -lopenblas -Wl,-Bdynamic
-g++ main.cc -std=c++17 -mfma -O3 -Ofast -ffast-math -fopenmp -I./include
+#g++ main.cc  -std=c++17 -I./include  -Wl,-Bstatic -mfma -O3 -Ofast -ffast-math  -Wl,-Bdynamic -fopenmp
+g++  main.cc -std=c++17 -mfma -O3 -Ofast -ffast-math -fopenmp -I./include
